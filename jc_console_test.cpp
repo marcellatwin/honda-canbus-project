bcec9e6a58e5a85fd6087e9a70554cca1c955a6c
marcellatwin <56375636+marcellatwin@users.noreply.github.com>
2019-10-16 11:20:28 -0700

    Update jc_console_test.cpp

--- jc_console_test.cpp
+++ jc_console_test.cpp
@@ -9,46 +9,33 @@
 // g++ -Wall -Wextra -O3 /home/pi/jc_main/src/jc_console_test.cpp -o /home/pi/jc_main/jc_test -lncurses
 
 #include <iostream>
-#include <curses.h>
-//#include "wiringPi.h"
+#include <ncurses.h>
 
 using namespace std;
 
-/*
-void row_mv (int n)
-{
-    for (int i = 0; i < n; i++)
-        cout << endl;
-}
-*/
 int main(void) {
     
     // Initilize curses variables
     initscr();
-    nodelay(stdscr, TRUE);
-    clear();
+    //nodelay(stdscr, TRUE);
+    //clear();
     curs_set(0);
-
-    printf("Start\n");
 
     // Try printing to the console
     int row = 1;
     
-    mvprintw(1, 1, "Thottle (%%):");
-    mvprintw(2, 1, "Thottle flag:");
-    mvprintw(3, 1, "Thottle Comp:");
-    /*
-    cout << "Start" << row_mv(1);
-    cout << "Thottle (%%):" << row_mv(1);
-    cout << "Thottle flag:" << row_mv(1);
-    cout << "Thottle Comp:" << row_mv(1);
-    */
+    mvprintw(row++, 1, "Thottle (%%):");
+    mvprintw(row++, 1, "Thottle flag:");
+    mvprintw(row++, 1, "Thottle Comp:");
+    
+    refresh();
+    
     // Wait for user to hit enter
     getchar();
     
     // Reset screen and show the curser
-    endwin();
     curs_set(1);
+    endwin();
 
     return 0;
 }
